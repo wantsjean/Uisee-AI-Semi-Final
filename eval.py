617bfa9903e99b20e7306479a59a97853ee43944
wantsjean <wantsjean@gmail.com>
2019-11-25 16:19:10 +0800

    add model loader train eval

--- eval.py
+++ eval.py
@@ -4,10 +4,6 @@ from PIL import Image
 import numpy as np
 import usimpy
 
-def mkdir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-
 # connection
 id = usimpy.UsimCreateApiConnection("127.0.0.1", 17771, 5000, 10000)
 
@@ -17,7 +13,7 @@ print (ret)
 
 ## control
 control = usimpy.UsimSpeedAdaptMode()
-control.expected_speed = 0.8 # m/s
+control.expected_speed = 1 # m/s
 control.steering_angle = 0.0 # angle
 control.handbrake_on = False
 ## action
@@ -30,10 +26,6 @@ image = usimpy.UsimCameraResponse()
 count = 0
 time1 = 0
 time2 = 0
-root_dir = './dataset/'
-img_dir = root_dir + 'img_r/'
-mkdir(img_dir)
-
 while(1):
     # control vehicle via speed & steer
     ret = usimpy.UsimSetVehicleControlsBySA(id, control)
@@ -47,13 +39,6 @@ while(1):
     img = np.array(image.image[0:480*320*3])
     img = img.reshape((320, 480, 3))
     img_PIL = Image.fromarray(np.uint8(img))
-    img_PIL.save(img_dir+str(action.time_stamp)+'_'+('%010d'%count)+'.png', 'png')
-    # save pose & action
-    with open(root_dir + 'action_pose_r.txt', 'a+') as f:
-        f.write('%d %d %.6f %.6f %.6f %.6f %.6f %.6f %.6f %.6f\n' % (count, action.time_stamp, action.pose.position.x_val, \
-                 action.pose.position.y_val, action.pose.position.z_val, action.pose.rotation.x_val, action.pose.rotation.y_val, \
-                 action.pose.rotation.z_val, action.steering_angle, action.forward_speed))
-    # auxiliary info
     time1 = action.time_stamp
     print ('time: %d, steer: %.6f, speed: %.6f, time_gap: %d, collision: %d, collision_time: %d' % (
             action.time_stamp, action.steering_angle, action.forward_speed, (time1-time2), collision.is_collided, collision.time_stamp))
